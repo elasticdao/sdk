3354a7af3e7fe23bce40d0d2142910bf4c4f2ef0
alleycatdev <alleycatdev@protonmail.com>
2020-12-03 14:49:21 -0500

    tweaks

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,5 @@
 /* eslint global-require: 0 */
 import commonjs from '@rollup/plugin-commonjs';
-import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
 import json from '@rollup/plugin-json';
 import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
