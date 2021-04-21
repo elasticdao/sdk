8cc0f34093be2070b99f0ae6014c3dc977ee0082
Dan Matthews <dan@cryptobuilders.io>
2021-04-21 23:05:34 +0300

    naming and efficiency improvements

--- src/MulticallQueue.js
+++ src/MulticallQueue.js
@@ -2,7 +2,7 @@
 import { ethers } from 'ethers';
 import MulticallABI from './abis/Multicall.json';
 
-const aggOut = [
+const aggregateOutputs = [
   {
     name: 'blockNumber',
     type: 'uint256',
@@ -15,8 +15,21 @@ const aggOut = [
 
 export default class MulticallQueue {
   constructor(sdk) {
-    this._sdk = sdk;
+    this._abiCoder = new ethers.utils.AbiCoder();
     this._entries = [];
+    this._multicall = new ethers.Contract(
+      '0xeefba1e63905ef1d7acba5a8513c70307c1ce441',
+      MulticallABI,
+    );
+    this._sdk = sdk;
+  }
+
+  get abiCoder() {
+    return this._abiCoder;
+  }
+
+  get multicall() {
+    return this._multicall;
   }
 
   get sdk() {
@@ -27,7 +40,6 @@ export default class MulticallQueue {
     const calls = [];
     const callbacks = [];
     const outputs = [];
-    const abiCoder = new ethers.utils.AbiCoder();
     let entry;
 
     while ((entry = this._entries.pop()) && entry) {
@@ -38,19 +50,17 @@ export default class MulticallQueue {
     }
 
     if (calls.length > 0) {
-      const contract = new ethers.Contract(
-        '0xeefba1e63905ef1d7acba5a8513c70307c1ce441',
-        MulticallABI,
+      const { data, to } = await this.multicall.populateTransaction.aggregate(
+        calls,
+        {
+          gasLimit: 0,
+        },
       );
+      const response = await this.sdk.provider.call({ data, to });
+      const decoded = this.abiCoder.decode(aggregateOutputs, response);
 
-      const { data, to } = await contract.populateTransaction.aggregate(calls, {
-        gasLimit: 0,
-      });
-      const response = await this._sdk.provider.call({ data, to });
-      const results = abiCoder.decode(aggOut, response);
-
-      for (let i = 0; i < results.returnData.length; i += 1) {
-        const result = abiCoder.decode(outputs[i], results.returnData[i]);
+      for (let i = 0; i < decoded.returnData.length; i += 1) {
+        const result = this.abiCoder.decode(outputs[i], decoded.returnData[i]);
         callbacks[i](outputs[i].length === 1 ? result[0] : result);
       }
     }
