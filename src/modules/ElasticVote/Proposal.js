9d142535b873fbe01bb5129ab5f355c4b9cd4350
0xean <0xean.eth@gmail.com>
2021-09-25 16:53:05 -0600

    modifications for new node (#90)

--- src/modules/ElasticVote/Proposal.js
+++ src/modules/ElasticVote/Proposal.js
@@ -36,7 +36,7 @@ export default class Proposal extends Base {
   }
 
   get abstain() {
-    return BigNumber(this._raw.abstain);
+    return BigNumber(this._raw.abstainCount);
   }
 
   get active() {
@@ -71,10 +71,6 @@ export default class Proposal extends Base {
     return this.api.space;
   }
 
-  get finalized() {
-    return this._raw.finalized;
-  }
-
   get id() {
     return this._raw.id;
   }
@@ -92,7 +88,7 @@ export default class Proposal extends Base {
   }
 
   get no() {
-    return BigNumber(this._raw.no);
+    return BigNumber(this._raw.noCount);
   }
 
   get pending() {
@@ -138,7 +134,7 @@ export default class Proposal extends Base {
   }
 
   get yes() {
-    return BigNumber(this._raw.yes);
+    return BigNumber(this._raw.yesCount);
   }
 
   action(action) {
@@ -196,6 +192,12 @@ export default class Proposal extends Base {
     const { value } = this.action(action);
 
     const signature = await this.sdk.signMessage(value);
+  
+    const requestBody = {
+      author: address,
+      signature: signature,
+      ...value
+    }
     const response = await this.fetch(this.nodeUrl, {
       method: 'POST',
       mode: 'cors',
@@ -203,14 +205,8 @@ export default class Proposal extends Base {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({
-        action,
-        address,
-        proposal: value,
-        signature,
-      }),
+      body: JSON.stringify(requestBody),
     });
-
     return response.json();
   }
 
