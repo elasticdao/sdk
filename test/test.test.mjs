ef977a34c5fce44460fdee3803efaf55c0289acf
0xean <0xean.eth@gmail.com>
2021-10-18 13:55:02 -0600

    adds missing files

---
+++ test/test.test.mjs
@@ -0,0 +1,12 @@
+import ElasticRewardsClass from '../dist/index.js';
+import chai from "chai";
+const expect = chai.expect;
+
+describe("test", function() {
+  
+
+
+  it("it works", async function() {
+    
+  });
+});
